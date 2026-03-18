51fa5722ee6e6e5821a7c858d5f7de12024dbfe7
fonikov <fonikovofoni@gmail.com>
2026-03-18 15:34:33 +0700

    fix: make external vless module globally available

--- src/modules/external-vless/external-vless.module.ts
+++ src/modules/external-vless/external-vless.module.ts
@@ -1,10 +1,11 @@
 import { CqrsModule } from '@nestjs/cqrs';
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 
 import { ExternalVlessController } from './external-vless.controller';
 import { ExternalVlessSyncTask } from './external-vless-sync.task';
 import { ExternalVlessService } from './external-vless.service';
 
+@Global()
 @Module({
     imports: [CqrsModule],
     controllers: [ExternalVlessController],
